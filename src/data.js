c59dda93e8f9da0b90fc09d8c8e64f40947b596e
myschproj <150742169+myschproj@users.noreply.github.com>
2024-01-24 12:00:32 +0300

    Update data.js

--- src/data.js
+++ src/data.js
@@ -13,11 +13,11 @@ function chillHop() {
       active: true,
     },
     {
-      name: "Daylight",
+      name: "Masunya",
       cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
-      artist: "Aiguille",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9272",
+        "https://raw.githubusercontent.com/myschproj/music/main/audios/IMG_9016.jpeg",
+      artist: "AtdaiHleb x NecoArc",
+      audio: "https://github.com/myschproj/music/raw/main/audios/ATDAI-HLEB-%D0%9C%D0%90%D0%A1%D0%AE%D0%9D%D0%AF-prod-white.m4a",
       color: ["#EF8EA9", "#ab417f"],
       id: uuidv4(),
       active: false,
