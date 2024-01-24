c4af5722129a5395a1a334e6fa5cdedd7b45a47a
myschproj <150742169+myschproj@users.noreply.github.com>
2024-01-24 21:58:57 +0300

    Update data.js

--- src/data.js
+++ src/data.js
@@ -73,6 +73,16 @@ function chillHop() {
       active: false,
     },
     {
+      name: "Supernatural",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+      artist: "Aluna&George",
+      audio: "https://github.com/myschproj/music/raw/main/audios/AlunaGeorge%20–%20Supernatural.m4a",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
       name: "Under the City Stars",
       cover:
         "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
